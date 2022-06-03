e61ef1c8b6e31cd5b2e4a52e7ee458802670db85
Zufan Hadgu <zufangebrehiwot10@gmail.com>
2022-06-02 20:50:54 -0700

    task5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/** 
+ * Description: main -print ou to stdout
+ * Return: 0 if sucess.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+		return (0);
+}
