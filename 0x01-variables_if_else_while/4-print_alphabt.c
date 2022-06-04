a1c29a86e7fefd267b7cfe0e03a0090d04eb5fb0
Zufan Hadgu <zufangebrehiwot10@gmail.com>
2022-06-04 15:22:32 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main-Entry point
+ *
+ *Return: always 0(success)
+ */
+int main(void)
+{
+	char alf = 'a';
+
+	while (alf <= 'z')
+	{
+		if (alf != 'e' && (alf != q))
+		{
+			putchar(alf);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
