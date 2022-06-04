f92483f3d131a4dc9513e93ee8a5cc95c9eeef2d
Zufan Hadgu <zufangebrehiwot10@gmail.com>
2022-06-04 14:13:32 -0700

    another commit for task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main-Entry point
+ *
+ *Return: always 0(succss)
+ */
+int main(void)
+{
+	int a;
+	for (a = 0; a < 10; a++)
+	{
+		printf("%d\n", a);
+	}
+	return (0);
+}
