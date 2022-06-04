41761e0ae7702d6ea7d3301cda4617bf6cb483b4
Zufan Hadgu <zufangebrehiwot10@gmail.com>
2022-06-04 13:26:18 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *main-Entry point 
+ *
+ *Return: Always 0(success)
+ */
+int main(void)
+{
+	char alf;
+
+	for (alf ='a'; alf <= 'z'; alf++)
+	{
+		putchar(alf);
+	}
+	for (alf ='A'; alf <= 'Z'; alf++)
+	{
+		putchar(alf);
+	}
+	putchar('\n');
+	return (0);
+}
