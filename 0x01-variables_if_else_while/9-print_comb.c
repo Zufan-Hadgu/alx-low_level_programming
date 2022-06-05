7985e80af786b21290c4c0cd49bc4618d105f06f
Zufan Hadgu <zufangebrehiwot10@gmail.com>
2022-06-05 12:40:28 -0700

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,11 +8,14 @@ int main(void)
 {
 	int i;
 
-	for (i = 48; i < 58; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
-		if (i < 58)
+		putchar(i);
+
+		if (i != '9')
 		{
-			putchar(', ');
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
