569aa66a192a149ef8d64249d0ae877b9b8aad3d
Zufan Hadgu <zufangebrehiwot10@gmail.com>
2022-06-05 12:59:05 -0700

    another commit

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ *main-Entry point
+ *
+ *Return: always 0 (success)
+ */
+int main(void)
+{
+	int c;
+	int d;
+
+	for (c = 0; c < 100; ++c)
+	{
+		for (d = 0;d < 100; ++d)
+		{
+			if (d > c)
+			{
+				putchar(c / 10 + '0');
+				putchar(c % 10 + '0');
+				putchar(' ');
+				putchar(d / 10 + '0');
+				putchar(d % 10 + '0');
+				if (c == 98 && d == 99)
+				{
+					continue;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
